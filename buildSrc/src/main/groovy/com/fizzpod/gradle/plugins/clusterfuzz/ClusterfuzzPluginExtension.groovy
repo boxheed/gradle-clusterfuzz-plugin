53fdf176a3bf5c19b83d1f1843e140fbc84d05a2
boxheed <boxheed@users.noreply.github.com>
2022-09-30 22:07:56 +0100

    Added flags and options on to the extension

--- buildSrc/src/main/groovy/com/fizzpod/gradle/plugins/clusterfuzz/ClusterfuzzPluginExtension.groovy
+++ buildSrc/src/main/groovy/com/fizzpod/gradle/plugins/clusterfuzz/ClusterfuzzPluginExtension.groovy
@@ -4,6 +4,6 @@ public class ClusterfuzzPluginExtension {
 
 	String language = "jvm"
 	String[] flags = []
-	Map<String, String> options = ["-runs": "100", "-timeout":"10", "-max_total_time":"300"]
+	Map<String, String> options = ["-runs": "100", "-timeout":"10", "-max_total_time":"300", "--jvm_args":"-Xmx2048m"]
 
 }
