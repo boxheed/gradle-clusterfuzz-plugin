78ee53eb00c1c549b3d3a57465560c7b8747a532
boxheed <boxheed@users.noreply.github.com>
2022-11-12 08:33:20 +0000

    Renamed clusterfuzz test case

--- src/clusterfuzz/java/com/fizzpod/gradle/plugins/clusterfuzz/ClusterfuzzPluginDefinitionFuzzTest.java
+++ src/clusterfuzz/java/com/fizzpod/gradle/plugins/clusterfuzz/ClusterfuzzPluginDefinitionFuzzTest.java
@@ -2,7 +2,7 @@ package com.fizzpod.gradle.plugins.clusterfuzz;
 
 import com.code_intelligence.jazzer.api.FuzzedDataProvider;
 
-public class ClusterfuzzPluginExtensionFuzzTest {
+public class ClusterfuzzPluginDefinitionFuzzTest {
 
     public static void fuzzerTestOneInput(FuzzedDataProvider data) {
         ClusterfuzzTestDefinition test = new ClusterfuzzTestDefinition();
