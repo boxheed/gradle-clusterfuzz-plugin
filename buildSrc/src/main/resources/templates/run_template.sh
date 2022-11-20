fe43a2532eeef20fe44ec02e24069b6dad0e4b2d
boxheed <boxheed@users.noreply.github.com>
2022-11-20 22:27:44 +0000

    Updated script to fail on error

--- buildSrc/src/main/resources/templates/run_template.sh
+++ buildSrc/src/main/resources/templates/run_template.sh
@@ -33,8 +33,8 @@ echo -e  "###################################################"
 set -e
 
 find ../ -name 'runTest.sh' | while read line; do
-    echo "Processing file '$line'"
-    bash $line
+    echo "Processing file '\$line'"
+    bash \$line
 done
 
 
